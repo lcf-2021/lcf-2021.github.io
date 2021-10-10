46374519720583edbe643c14f7c1b83184197568
sigelson <32645870+sigelson@users.noreply.github.com>
2021-10-10 23:13:07 +0800

    Update sponsor columns responsive

---
+++ transfergit.bat
@@ -0,0 +1,5 @@
+xcopy "C:\Users\gelso\Desktop\UXUI\website\local" "C:\Users\gelso\Desktop\UXUI\website\github" /h /i /c /k /e /r /y
+cd C:\Users\gelso\Desktop\UXUI\website\github
+git add .
+git commit
+git push
