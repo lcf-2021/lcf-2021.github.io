8594c5dba7cac5bd971cb894d45b70f1afb17b09
sigelson <32645870+sigelson@users.noreply.github.com>
2021-10-10 23:18:39 +0800

    Remove unused line

--- transfergit.bat
+++ transfergit.bat
@@ -1,5 +1,8 @@
+@echo off
+@echo Copying files to github folder...
 xcopy "C:\Users\gelso\Desktop\UXUI\website\local" "C:\Users\gelso\Desktop\UXUI\website\github" /h /i /c /k /e /r /y
 cd C:\Users\gelso\Desktop\UXUI\website\github
+@echo Commiting files  and pushing updates to the repository...
 git add .
 git commit
 git push
