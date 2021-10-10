7817580fd8ff4bd18976f2f46c3f5feb2f3e2e6d
sigelson <32645870+sigelson@users.noreply.github.com>
2021-10-10 23:20:38 +0800

    Remove transfergit file

--- transfergit.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-@echo Copying files to github folder...
-xcopy "C:\Users\gelso\Desktop\UXUI\website\local" "C:\Users\gelso\Desktop\UXUI\website\github" /h /i /c /k /e /r /y
-cd C:\Users\gelso\Desktop\UXUI\website\github
-@echo Commiting files  and pushing updates to the repository...
-git add .
-git commit
-git push
